038d3617f5f2aef3cd09ae1eabeed6304e80cb4d
Densch <dennis.schaffner@gmx.de>
2024-05-28 17:56:28 +0200

    [ADD] .gitignore, Unity Porjekt und WWise Sachen erstellt

---
+++ Wwise Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_1_52__TRAINERS__EYES_MEET__CLERK___ = 831395321U;
+        static const AkUniqueID PLAY_2_23__OPELUCID_CITY_GYM__POK_MON_BLACK_ = 2991142287U;
+        static const AkUniqueID PLAY_SATANLAUGH1 = 1899080551U;
+        static const AkUniqueID PLAYINTRO = 781908707U;
+        static const AkUniqueID SET_STATE_ALIVE = 1814951015U;
+        static const AkUniqueID SET_STATE_DEAD = 3796261246U;
+        static const AkUniqueID SET_SWITCH_ALIVE = 1323350628U;
+        static const AkUniqueID SET_SWITCH_DEAD2 = 365610735U;
+        static const AkUniqueID STOP_EVENT = 2904546416U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ALIVE
+        {
+            static const AkUniqueID GROUP = 655265632U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace ALIVE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace NEW_SWITCH_GROUP
+        {
+            static const AkUniqueID GROUP = 2250165792U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ALIVE2 = 3689657362U;
+                static const AkUniqueID DEAD2 = 1047719995U;
+            } // namespace SWITCH
+        } // namespace NEW_SWITCH_GROUP
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SPEED = 640949982U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID NEW_TRIGGER = 4163741908U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
